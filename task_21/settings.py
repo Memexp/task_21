539594695a4d01f0ba8d44d41be2ca8e3fb7a6be
Maram Kamil <maram.kamil@gmail.com>
2019-02-23 20:53:03 +0300

    test

--- task_21/settings.py
+++ task_21/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '104.248.245.246']
+ALLOWED_HOSTS = ['127.0.0.1', '207.154.230.70']
 
 
 # Application definition
@@ -93,7 +93,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
             'NAME': 'django',
             'USER': 'django',
-            'PASSWORD': '2f5a5892cb7e54bac2083d82a508573a',
+            'PASSWORD': '52f50fd6b198c0118f8b032ff10c84f9',
             'HOST': 'localhost',
             'PORT': '',
         }
